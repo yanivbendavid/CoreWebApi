6308dcb86872ff9845e5b74e865c80a24c6c99d3
Yaniv Ben David <yanivbd@gmail.com>
2022-05-08 16:29:01 +0300

    API Init

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,43 @@
+﻿using CoreWebApi.Data;
+using CoreWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //[controller] is taking the name of contacts
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext _dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+            return Ok(await _dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                FullName = addContactRequest.FullName,
+                Email = addContactRequest.Email,
+                Phone = addContactRequest.Phone,
+                Address = addContactRequest.Address
+            };
+          
+            await _dbContext.Contacts.AddAsync(contact);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+    }
+}
---
+++ Data/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CoreWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebApi.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
