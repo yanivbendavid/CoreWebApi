ccbca524a6ce62e95111529e7de796ce85673b58
Yaniv Ben David <yanivbd@gmail.com>
2022-05-08 17:23:32 +0300

    Added PUT to contact api

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -33,8 +33,33 @@ namespace CoreWebApi.Controllers
                 Phone = addContactRequest.Phone,
                 Address = addContactRequest.Address
             };
-          
+
             await _dbContext.Contacts.AddAsync(contact);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest)
+        {
+            if (id == Guid.Empty)
+            {
+                return NotFound();
+            }
+
+            var contact = await _dbContext.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            contact.FullName = updateContactRequest.FullName;
+            contact.Email = updateContactRequest.Email;
+            contact.Phone = updateContactRequest.Phone;
+            contact.Address = updateContactRequest.Address;
+
             await _dbContext.SaveChangesAsync();
 
             return Ok(contact);
